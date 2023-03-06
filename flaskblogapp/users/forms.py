e8d6894dbc8b3c1c9d87eb8183f04e9c3439bb66
Junior PC <49306278+jvniorrr@users.noreply.github.com>
2023-03-05 17:40:00 -0800

    Update forms.py

--- flaskblogapp/users/forms.py
+++ flaskblogapp/users/forms.py
@@ -50,11 +50,11 @@ class UpdateAccountForm(FlaskForm):
             user = User.query.filter_by(username=username.data).first()
             if user:
                 raise ValidationError("That username is taken. Please choose a different one.")
-        def validate_email(self, email):
-            if email.data != current_user.username:
-                user = User.query.filter_by(email=email.data).first()
-                if user:
-                    raise ValidationError("That email is already being used. Please choose a different one.")
+    def validate_email(self, email):
+        if email.data != current_user.username:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError("That email is already being used. Please choose a different one.")
 
 class RequestResetForm(FlaskForm):
     email = StringField("Email",
